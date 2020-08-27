6f045927da028f15d3e52e1dc0c50607bb0cd066
mili12321 <milita.malzev@gmail.com>
2020-08-27 19:36:35 +0300

    table_changes

--- apps/misterEmail/cmps/EmailPreview.jsx
+++ apps/misterEmail/cmps/EmailPreview.jsx
@@ -69,25 +69,27 @@ export class EmailPreview extends React.Component {
         </tr>
         <tr>
           <td colSpan='5' className={`sliding-email ${this.state.showMailbyClick}`}>
-              <div className="sliding-email-header">
-                  <div className="sliding-email-subject">{ this.props.email.subject }</div>
-                  <div className="sliding-email-options">
+              <div className="continer-td">
+                  <div className="sliding-email-header">
+                      <div className="sliding-email-subject">{ this.props.email.subject }</div>
+                      <div className="sliding-email-options">
 
-                    <div className="round-div">
-                      <i className="fas fa-trash" onClick={ () => this.onRemoveEmail(this.props.idx) }></i>
-                    </div>
-                    <div className="round-div">
-                      <Link to={`/EmailApp/${this.props.email.id}`}>
-                        <i className="fas fa-expand"></i>
-                      </Link>
-                    </div>
-                   
+                        <div className="round-div">
+                          <i className="fas fa-trash" onClick={ () => this.props.onRemoveEmail(this.props.idx) }></i>
+                        </div>
+                        <div className="round-div">
+                          <Link to={`/EmailApp/${this.props.email.id}`}>
+                            <i className="fas fa-expand"></i>
+                          </Link>
+                        </div>
+
+                      </div>
                   </div>
+                  <div className="sliding-email-sender"><span className="sender-name">{this.props.email.sender}</span> {`<${this.props.email.sender}@gmail.com>`}</div>
+                  <div className="sliding-email-body">{ this.props.email.body }
+                  Lorem, ipsum dolor sit amet consectetur adipisicing.</div>
+
               </div>
-              <div className="sliding-email-sender"><span className="sender-name">{this.props.email.sender}</span> {`<${this.props.email.sender}@gmail.com>`}</div>
-              <div className="sliding-email-body">{ this.props.email.body }
-              Lorem, ipsum dolor sit amet consectetur adipisicing.</div>
-            
           </td>
         </tr>
       </React.Fragment>
