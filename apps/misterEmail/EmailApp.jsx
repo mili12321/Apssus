9c1279dd16f5cb83b97718d0e273279da01e5a6c
mili12321 <milita.malzev@gmail.com>
2020-08-27 15:08:55 +0300

    inputs to mail

--- apps/misterEmail/EmailApp.jsx
+++ apps/misterEmail/EmailApp.jsx
@@ -1,7 +1,11 @@
 import { emailService } from '../../services/email-service.js'
 import { EmailList } from 'cmps/EmailList.jsx';
+
+
+
 export class EmailApp extends React.Component {
     state = {
+        emailToAdd: emailService.getEmpty(),
         emails: [],
     }
     componentDidMount() {
@@ -13,18 +17,44 @@ export class EmailApp extends React.Component {
                 this.setState({ emails })
             })
     }
+    addEmail = () =>{
+        console.log('adding email')
+        emailService.add(this.state.emailToAdd)
+        this.setState({emailToAdd:emailService.getEmpty()})
+        this.loadEmails();
+       
+    }
+    onInputChange=(ev)=>{
+        console.log('input', ev.target.name)
+        console.log('changed', ev.target.value)
+        this.setState({emailToAdd: {...this.state.emailToAdd, [ev.target.name]:ev.target.value}})
+
+    }
     render() {
 
         return (
             <div className="email-app-container">
+                
                 <div className="email-options-container">
-                    <div className="email-option new-mail">+ Compose</div>
+                    <input name='sender' value={this.state.emailToAdd.sender} 
+                        placeholder='sender name'
+                        type="text" onChange={this.onInputChange}
+                    />
+                    <input name='subject' value={this.state.emailToAdd.subject}
+                        placeholder='subject' 
+                        type="text" onChange={this.onInputChange}
+                    />
+                    <input name="body" value={this.state.emailToAdd.body}
+                        placeholder='body' 
+                        type="text" onChange={this.onInputChange}
+                    />
+                    <div className="email-option new-mail" onClick={this.addEmail}>+ Compose</div>
                     <div className="email-option inbox">Inbox</div>
                     <div className="email-option starred">Starred</div>
                     <div className="email-option sent-mail">Sent mail</div>
                     <div className="email-option drafs">Drafs</div>
                 </div>
-                <EmailList emails={ this.state.emails } />
+                <EmailList emails={ this.state.emails }/>
             </div>
         )
     }
--- apps/misterEmail/EmailDetails.jsx
+++ apps/misterEmail/EmailDetails.jsx
@@ -1,10 +1,16 @@
+const { Link } = ReactRouterDOM
 export class EmailDetails extends React.Component {
+    state = {
+        email: null
+    }
+
     
     render() {
 
         return (
             <div>
                {this.props.email}
+               <Link to="/EmailApp">back</Link>
             </div>
         )
     }
